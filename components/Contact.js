2bdf75261f821235770fb78035729cfceede86f9
David Krause <davidkrause@Davids-MBP.lan1>
2018-12-09 20:28:50 -0600

    Add click action to mobile menu icon Add style for large screens for contact section

--- components/Contact.js
+++ components/Contact.js
@@ -1,19 +1,21 @@
 const Contact = () => (
-  <section classNameName="contact" id="contact">
+  <section className="contact" id="contact">
     <div className="container">
       <div className="row">
-        <div className="col-xs-12">
-          <h1>Contact Us</h1>
-        </div>
-        <div className="col-xs-12">
-          <h2 className="phone-label">Call</h2>
-          <p className="phone">432-889-8458</p>
+        <div className="contact-info">
+          <div className="col-xs-12">
+            <h1>Contact Us</h1>
+          </div>
+          <div className="col-xs-12">
+            <h2 className="phone-label">Call</h2>
+            <p className="phone">432-889-8458</p>
+          </div>
         </div>
         <div className="col-xs-12 email">
           <h2>Send An Email</h2>
           <form name="contact-form" noValidate>
             <div className="form-group">
-              <label for="name" className="control-label">Name</label>
+              <label htmlFor="name" className="control-label">Name</label>
               <input
                 id="name"
                 name="name"
@@ -23,7 +25,7 @@ const Contact = () => (
               />
             </div>
             <div className="form-group">
-              <label for="email" className="control-label">Email</label>
+              <label htmlFor="email" className="control-label">Email</label>
               <input
                 id="email"
                 name="email"
@@ -33,7 +35,7 @@ const Contact = () => (
               />
             </div>
             <div className="form-group">
-              <label for="comment" className="control-label">Comment</label>
+              <label htmlFor="comment" className="control-label">Comment</label>
               <textarea
                 id="comment"
                 className="form-control"
@@ -43,7 +45,9 @@ const Contact = () => (
               />
             </div>
             <div className="section-button">
-              <a href="/service" className="btn btn-default btn-lg">Send Email</a>
+              <a href="/service" className="btn btn-default btn-lg">
+                Send Email
+              </a>
             </div>
           </form>
         </div>
--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,58 @@
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import {Component} from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
-const Header = () => (
-    <header>
-      <nav className="navbar navbar-inverse" role="navigation">
-        <div className="navbar-logo">
-          <AnchorLink  href="#__next">
-            <img
-              src="/static/signature-theaters-logo.png"
-              alt="Signature Theaters logo"
-            />
-          </AnchorLink >
-        </div>
-        <a className="navbar-toggle">
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </a>
-        <ul className="nav navbar-nav">
-          <span className="icon-bar" />
-          <li>
-            <AnchorLink href="#service">
-              Service
+class Header extends Component {
+  state = {
+    open: false,
+  };
+  handleClick = () => {
+    this.setState (state => ({open: !state.open}));
+  };
+  render () {
+    const {open} = this.state;
+    return (
+      <header>
+        <nav className="navbar navbar-inverse" role="navigation">
+          <div className="navbar-logo">
+            <AnchorLink href="#__next">
+              <img
+                src="/static/signature-theaters-logo.png"
+                alt="Signature Theaters logo"
+              />
             </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink  href="#experience" target="_self">
-              Experience
-            </AnchorLink >
-          </li>
-          <li>
-            <AnchorLink  href="#products" target="_self">
-              Products
-            </AnchorLink>
-          </li>
-          <li>
-            <AnchorLink href="#contact" target="_self">
-              Contact us
-            </AnchorLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
-)
+          </div>
+          <a onClick={this.handleClick} className="navbar-toggle">
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </a>
+          <ul className={open ? 'nav navbar-nav new-class' : 'nav navbar-nav'}>
+            <span className="icon-bar" />
+            <li>
+              <AnchorLink href="#service">
+                Service
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#experience" target="_self">
+                Experience
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#products" target="_self">
+                Products
+              </AnchorLink>
+            </li>
+            <li>
+              <AnchorLink href="#contact" target="_self">
+                Contact us
+              </AnchorLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
 
 export default Header;
