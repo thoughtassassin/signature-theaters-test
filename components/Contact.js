ef76d2138f989c6658230eadf220b476aad0acdf
David Krause <davidkrause@Davids-MBP.lan1>
2018-12-17 20:48:06 -0600

    Add mailgun dependency Add mailgun method to send email Add validation messages to Contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,59 +1,96 @@
 import {emailValidation} from '../utils/emailValidation';
+const initialState = {
+  name: {
+    value: '',
+    validation: null,
+    errorMessage: 'Name is required.',
+    error: false,
+  },
+  email: {
+    value: '',
+    validation: emailValidation,
+    errorMessage: 'Email must be a valid email.',
+    error: false,
+  },
+  comment: {
+    value: '',
+    validation: null,
+    errorMessage: 'Comment is required.',
+    error: false,
+  },
+  errors: [],
+  messageSent: false,
+};
 
 class Contact extends React.Component {
-  state = {
-    name: {
-      value: '',
-      validation: null,
-      errorMessage: 'Name is required.',
-    },
-    email: {
-      value: '',
-      validation: emailValidation,
-      errorMessage: 'Email must be a valid email.',
-    },
-    comment: {
-      value: '',
-      validation: null,
-      errorMessage: 'Comment is required.',
-    },
-    errors: [],
-  };
+  state = initialState;
 
   handleChange = ({target: {name, value}}) => {
     const {[name]: element} = this.state;
     this.setState ({
-      [name]: {...element, value},
+      [name]: {...element, value, error: false},
     });
   };
+
+  closeAlert = () => {
+    this.setState({messageSent: false});
+  }
 
   handleSubmit = e => {
     e.preventDefault ();
     let errorCount = 0;
     this.setState ({errors: []});
     for (const field in this.state) {
-      if (field != 'errors') {
+      if (field !== 'errors' && field !== 'messageSent') {
         const {validation, value, errorMessage} = this.state[field];
         if (!value || (validation && !validation.test (value))) {
           errorCount++;
-          this.setState(state => ({errors: [...state.errors, errorMessage]}));
+          this.setState (state => ({
+            [field]: {...state[field], error: true},
+            errors: [...state.errors, errorMessage],
+          }));
         }
       }
     }
     if (errorCount === 0) {
       if (grecaptcha) {
-        grecaptcha.ready(() =>
+        grecaptcha.ready (() =>
           grecaptcha
-            .execute('6Lcdd4EUAAAAAHBWMAYgcS2KxkXt4_cc_1e6yIDa')
-            .then(token => {
-              fetch('/validate', {
+            .execute ('6Lcdd4EUAAAAAHBWMAYgcS2KxkXt4_cc_1e6yIDa')
+            .then (token => {
+              fetch ('/validate', {
                 method: 'post',
                 headers: {
                   'Content-Type': 'application/json; charset=utf-8',
                 },
                 body: JSON.stringify ({token}),
               })
-                .then (response => { if (response.ok) { return response.json() } })
+                .then (response => {
+                  if (response.ok) {
+                    fetch ('/mail', {
+                      method: 'post',
+                      headers: {
+                        'Content-Type': 'application/json; charset=utf-8',
+                      },
+                      body: JSON.stringify ({
+                        to: 'utpbart@yahoo.com',
+                        name: this.state.name.value,
+                        from: this.state.email.value,
+                        comment: this.state.comment.value,
+                      }),
+                    })
+                      .then (reponse => {
+                        if (response.ok) {
+                          return console.log ('Message Sent');
+                        } else {
+                          Promise.reject ('Message was not sent.');
+                        }
+                      })
+                      .then (() =>
+                        this.setState ({...initialState, messageSent: true})
+                      );
+                  }
+                })
                 .then (validation => console.log (validation));
             })
         );
@@ -62,7 +99,7 @@ class Contact extends React.Component {
   };
 
   render () {
-    const {name, email, comment} = this.state;
+    const {name, email, comment, messageSent} = this.state;
     return (
       <section className="contact" id="contact">
         <div className="container">
@@ -78,8 +115,20 @@ class Contact extends React.Component {
             </div>
             <div className="col-xs-12 email">
               <h2>Send An Email</h2>
+              {messageSent &&
+                <div class="alert alert-success" role="alert">
+                  Message sent!
+                  <button
+                    type="button"
+                    class="close"
+                    onClick={this.closeAlert}
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>}
               <form name="contact-form" noValidate onSubmit={this.handleSubmit}>
-                <div className="form-group">
+                <div className={name.error ? 'form-group error' : 'form-group'}>
                   <label htmlFor="name" className="control-label">Name</label>
                   <input
                     id="name"
@@ -91,8 +140,12 @@ class Contact extends React.Component {
                     onChange={this.handleChange}
                     required
                   />
+                  {name.error &&
+                    <div className="error-message"> {name.errorMessage}</div>}
                 </div>
-                <div className="form-group">
+                <div
+                  className={email.error ? 'form-group error' : 'form-group'}
+                >
                   <label htmlFor="email" className="control-label">Email</label>
                   <input
                     id="email"
@@ -104,8 +157,12 @@ class Contact extends React.Component {
                     onChange={this.handleChange}
                     required
                   />
+                  {email.error &&
+                    <div className="error-message"> {email.errorMessage}</div>}
                 </div>
-                <div className="form-group">
+                <div
+                  className={comment.error ? 'form-group error' : 'form-group'}
+                >
                   <label htmlFor="comment" className="control-label">
                     Comment
                   </label>
@@ -120,6 +177,10 @@ class Contact extends React.Component {
                     required
                     rows="7"
                   />
+                  {comment.error &&
+                    <div className="error-message">
+                      {' '}{comment.errorMessage}
+                    </div>}
                 </div>
                 <div className="section-button">
                   <button className="btn btn-default btn-lg">
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next ({dev});
 const handle = app.getRequestHandler ();
 const axios = require ('axios');
+const api_key = 'a9847e0916c10c874501b0747d7cd559-b3780ee5-6e8e540f';
+const DOMAIN = 'sandbox7cfd230b5f4d4c559a1da62437280e09.mailgun.org';
+const mailgun = require('mailgun-js')({apiKey: api_key, domain: DOMAIN});
 
 app
   .prepare ()
@@ -16,6 +19,24 @@ app
     server.get('/api', (req, res) => {
       res.send ({obj: 'I am the api.'});
     });
+
+    server.post('/mail', (req, res) => {
+      var data = {
+        from: `${req.body.name} <${req.body.from}>`,
+        to: 'christopher@signaturetheaters.com',
+        subject: 'Comment from website',
+        text: req.body.comment
+      };
+
+      console.log(data)
+      
+      mailgun.messages().send(data, function (error, body) {
+        console.log('body: ', body);
+        console.log('error: ', error);
+      });
+      
+      res.send('Mail sent.')
+    })
 
     server.post ('/validate', (req, res) => {
       const secret = '6Lcdd4EUAAAAALqy8qZxS2IC7snG26921sHbOvZM';
