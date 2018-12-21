85eccd425667bfeee2ead8b100d47d03dabae0c8
David Krause <davidkrause@Davids-MBP.lan1>
2018-12-20 21:36:00 -0600

    Fix invalid jsx attribute Correct bottom position of chatbox

--- icons/ChatIcon.js
+++ icons/ChatIcon.js
@@ -1,7 +1,7 @@
 const ChatIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 35.715" width="40px" height="35.715px">
         <title>Chat Icon</title>
-        <path class="cls-1" fill="#FFC629" d="M37,0H3A3,3,0,0,0,0,3V20a3,3,0,0,0,3,3h8.28L10.19,35.71,23,23H37a3,3,0,0,0,3-3V3A3,3,0,0,0,37,0Z"/>
+        <path className="cls-1" fill="#FFC629" d="M37,0H3A3,3,0,0,0,0,3V20a3,3,0,0,0,3,3h8.28L10.19,35.71,23,23H37a3,3,0,0,0,3-3V3A3,3,0,0,0,37,0Z"/>
     </svg>
 );
 
