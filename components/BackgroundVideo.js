29810ea140fcd76ca576d19ced25b4ab4b1585f8
David Krause <davidkrause@Davids-MBP.lan1>
2019-01-13 11:42:58 -0600

    Add background video

---
+++ components/BackgroundVideo.js
@@ -0,0 +1,13 @@
+class BackgroundVideo extends React.Component {
+  render () {
+    return (
+      <div className="fullscreen-bg">
+        <video loop muted autoPlay className="fullscreen-bg__video">
+          <source src="/static/sigtheaters_1280.mp4" type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+}
+
+export default BackgroundVideo;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import BackgroundVideo from '../components/BackgroundVideo';
 import Header from '../components/Header';
 import Service from '../components/Service';
 import Experience from '../components/Experience';
@@ -30,6 +31,7 @@ class Index extends React.Component {
           <script src="https://www.google.com/recaptcha/api.js?render=6Lcdd4EUAAAAAHBWMAYgcS2KxkXt4_cc_1e6yIDa" />
           <script data-cfasync="false" src="/static/chat.js" />
         </Head>
+        <BackgroundVideo />
         <Header />
         <Service />
         <Experience />
