5aab687b29618d6d1893b3b0a2e5be43f6d0f626
David Krause <davidkrause@Davids-MBP.lan1>
2019-01-20 12:57:30 -0600

    Add OpeningContent component Add Product logos

---
+++ components/OpeningContent.js
@@ -0,0 +1,11 @@
+const OpeningContent = () => (
+    <div className="opening-content">
+        <div>
+            <h1>Finer Home Systems</h1>
+            <div><ul><li>Home Automation</li><li>Theater</li><li>Surveillance &amp; Security</li></ul></div>
+        </div>
+    </div>
+);
+
+export default OpeningContent;
+
--- components/Products.js
+++ components/Products.js
@@ -2,60 +2,60 @@ const Products = () => (
     <section className="products" id="products">
       <div className="container">
         <h1>Signature Products</h1>
-        <div className="row products-list-item vertical-align">
-          <div className="col-sm-6 col-xs-12">
+        <div className="row products-list-item">
+          <div className="col-sm-4 col-xs-6 product">
             <img
-              src="/static/design.png"
+              src="/static/anthem-logo.svg"
+              alt="Anthem logo"
+            />
+          </div>
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/control4-logo.svg"
+              alt="Control4 logo"
+            />
+          </div>
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/klipsch-logo.svg"
+              alt="Klipsch logo"
+            />
+          </div>
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/lutron-logo.svg"
+              alt="Lutron logo"
+            />
+          </div>
+          <div className="col-sm-4 col-xs-6 product">
+            <img
+              src="/static/pakedge-logo.svg"
+              alt="Pakedge logo"
+            />
+          </div>
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/SI-logo.svg"
               alt="Blueprints of home"
             />
           </div>
-          <div className="col-sm-6 col-xs-12">
-            <h2>Audio</h2>
-            <p>
-              Customer service begins with a signature design tailored to your
-              personal preferences. Using sophisticated software to design the
-              outlets and tributaries of electrical work, Signature Theaters
-              provides a blueprint for the proposed layout of your home theater.
-            </p>
-          </div>
-        </div>
-        <div className="row products-list-item vertical-align order-right">
-          <div className="col-sm-6 col-xs-12">
-            <img
-              src="/static/home-security.png"
-              alt="home security camera"
-              className="right"
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/sonance-logo.svg"
+              alt="Sonance logo"
             />
           </div>
-          <div className="col-sm-6 col-xs-12">
-            <h2>Installation</h2>
-            <p>
-              Signature Theaters provides custom home installation of quality
-              home theater products, sound systems, and video surveillance for
-              the interior and exterior of your home. With 25 years serving
-              the Midland area and Cedia Certified professionals at your
-              service, custom home automation is a call away.
-            </p>
-
-          </div>
-        </div>
-        <div className="row products-list-item vertical-align">
-          <div className="col-sm-6 col-xs-12">
-            <img
-              src="/static/home-security.png"
-              alt="home security camera"
+          <div className="col-sm-4 col-xs-6 product">
+          <img
+              src="/static/sony-logo.svg"
+              alt="Sony logo"
             />
           </div>
-          <div className="col-sm-6 col-xs-12">
-            <h2>Support</h2>
-            <p>
-              Signature Theaters provides custom home installation of quality
-              home theater products, sound systems, and video surveillance for
-              the interior and exterior of your home. With 25 years serving
-              the Midland area and Cedia Certified professionals at your
-              service, custom home automation is a call away.
-            </p>
-
+          <div className="col-sm-4 col-xs-12 product last">
+          <img
+              src="/static/triad-logo.svg"
+              alt="Triad logo"
+            />
           </div>
         </div>
       </div>
