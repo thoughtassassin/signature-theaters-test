3f8952e471d2e2267f1a498b92821dd5b7ef1210
David Krause <davidkrause@Davids-MBP.lan1>
2019-04-14 19:37:00 -0500

    Add new product logos Make dealer status logos fill page

--- components/Products.js
+++ components/Products.js
@@ -48,6 +48,51 @@ const Products = () => (
             <img src="/static/triad-logo.svg" alt="Triad logo" />
           </a>
         </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.universalremote.com/" target="_blank">
+            <img src="/static/urc-logo.svg" alt="URC logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.avproedge.com/" target="_blank">
+            <img src="/static/avpro-logo.svg" alt="AV Pro logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.snapav.com/shop/en/snapav/wattbox" target="_blank">
+            <img src="/static/wattbox-logo.svg" alt="Wattbox logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.snapav.com/shop/en/snapav/binary" target="_blank">
+            <img src="/static/binary-logo.svg" alt="Binary logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://sound-machine.com/" target="_blank">
+            <img src="/static/soundmachine-logo.svg" alt="Soundmachine logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="http://www.ovrc.com/" target="_blank">
+            <img src="/static/ovrc-logo.svg" alt="OVRC logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.snapav.com/shop/en/snapav/strong" target="_blank">
+            <img src="/static/strong-logo.svg" alt="Strong logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://www.sunbritetv.com/" target="_blank">
+            <img src="/static/sunbrite-logo.svg" alt="Sunbrite logo" />
+          </a>
+        </div>
+        <div className="col-sm-4 col-xs-6 product">
+          <a href="https://nest.com/" target="_blank">
+            <img src="/static/nest-logo.svg" alt="Nest logo" />
+          </a>
+        </div>
       </div>
       <div className="dealer-status">
         <div>
