74791b59e1259d812703a9ef0c9d03c866e20be0
David Krause <davidkrause@Davids-MBP.lan1>
2020-01-23 18:50:47 -0600

    Add video

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -2,7 +2,9 @@ class BackgroundVideo extends React.Component {
   render() {
     return (
       <div className="fullscreen-bg">
-        <video loop muted autoPlay className="fullscreen-bg__video"></video>
+        <video loop muted autoPlay className="fullscreen-bg__video">
+          <source src="/static/sigtheaters_1280.mp4" type="video/mp4" />
+        </video>
       </div>
     );
   }
