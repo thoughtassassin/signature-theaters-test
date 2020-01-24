064c423d89df220a76ee2a3abdd8e070d2c020f5
David Krause <davidkrause@Davids-MBP.lan1>
2020-01-23 18:31:53 -0600

    Remove video

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -1,10 +1,8 @@
 class BackgroundVideo extends React.Component {
-  render () {
+  render() {
     return (
       <div className="fullscreen-bg">
-        <video loop muted autoPlay className="fullscreen-bg__video">
-          <source src="/static/sigtheaters_1280.mp4" type="video/mp4" />
-        </video>
+        <video loop muted autoPlay className="fullscreen-bg__video"></video>
       </div>
     );
   }
