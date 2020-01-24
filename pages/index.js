934c8c3a27fb8b25a74b4e7f609cdc659bbf46fe
David Krause <davidkrause@Davids-MBP.lan1>
2020-01-23 18:02:24 -0600

    Load scripts async Set Link to preload

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
-import Head from 'next/head';
-import BackgroundVideo from '../components/BackgroundVideo';
-import Header from '../components/Header';
-import Service from '../components/Service';
-import Projects from '../components/Projects';
-import Products from '../components/Products';
-import Contact from '../components/Contact';
-import '../stylesheets/bootstrap.css';
-import '../stylesheets/styles.css';
+import Head from "next/head";
+import BackgroundVideo from "../components/BackgroundVideo";
+import Header from "../components/Header";
+import Service from "../components/Service";
+import Projects from "../components/Projects";
+import Products from "../components/Products";
+import Contact from "../components/Contact";
+import "../stylesheets/bootstrap.css";
+import "../stylesheets/styles.css";
 
 class Index extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <Head>
@@ -24,7 +24,8 @@ class Index extends React.Component {
             name="apple-mobile-web-app-status-bar-style"
             content="black-translucent"
           />
-          <meta name="description" 
+          <meta
+            name="description"
             content="Signature Theaters designs and installs custom home theater and home automation systems. Serving the Permian Basin since 2008."
           />
           <script
@@ -38,14 +39,17 @@ class Index extends React.Component {
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'UA-134677065-1');
-          `,
+          `
             }}
           />
-          <script src="https://www.google.com/recaptcha/api.js?render=6Lcdd4EUAAAAAHBWMAYgcS2KxkXt4_cc_1e6yIDa" />
-          <script data-cfasync="false" src="/static/chat.js" />
+          <script
+            async
+            src="https://www.google.com/recaptcha/api.js?render=6Lcdd4EUAAAAAHBWMAYgcS2KxkXt4_cc_1e6yIDa"
+          />
+          <script async data-cfasync="false" src="/static/chat.js" />
           <link
+            rel="preload"
             href="https://fonts.googleapis.com/css?family=Play"
-            rel="stylesheet"
           />
         </Head>
         <BackgroundVideo />
