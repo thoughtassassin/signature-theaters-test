e45ccb2451a23a74be118435f591f652f5815cc8
David Krause <davidkrause@Davids-MBP.lan1>
2020-01-23 18:26:08 -0600

    Add rel="noopener" to links

--- components/Products.js
+++ components/Products.js
@@ -4,114 +4,160 @@ const Products = () => (
       <h1>Signature Products</h1>
       <div className="row products-list-item">
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.anthemav.com/" target="_blank">
+          <a href="https://www.anthemav.com/" target="_blank" rel="noopener">
             <img src="/static/anthem-logo.svg" alt="Anthem logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.control4.com/" target="_blank">
+          <a href="https://www.control4.com/" target="_blank" rel="noopener">
             <img src="/static/control4-logo.svg" alt="Control4 logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.klipsch.com/" target="_blank">
+          <a href="https://www.klipsch.com/" target="_blank" rel="noopener">
             <img src="/static/klipsch-logo.svg" alt="Klipsch logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="http://www.lutron.com/en-US/Pages/default.aspx" target="_blank">
+          <a
+            href="http://www.lutron.com/en-US/Pages/default.aspx"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/lutron-logo.svg" alt="Lutron logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.pakedge.com/" target="_blank">
+          <a href="https://www.pakedge.com/" target="_blank" rel="noopener">
             <img src="/static/pakedge-logo.svg" alt="Pakedge logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.screeninnovations.com/" target="_blank">
+          <a
+            href="https://www.screeninnovations.com/"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/SI-logo.svg" alt="Blueprints of home" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.sonance.com/" target="_blank">
+          <a href="https://www.sonance.com/" target="_blank" rel="noopener">
             <img src="/static/sonance-logo.svg" alt="Sonance logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.sony.com/" target="_blank">
+          <a href="https://www.sony.com/" target="_blank" rel="noopener">
             <img src="/static/sony-logo.svg" alt="Sony logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.triadspeakers.com/" target="_blank">
+          <a
+            href="https://www.triadspeakers.com/"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/triad-logo.svg" alt="Triad logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.universalremote.com/" target="_blank">
+          <a
+            href="https://www.universalremote.com/"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/urc-logo.svg" alt="URC logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.avproedge.com/" target="_blank">
+          <a href="https://www.avproedge.com/" target="_blank" rel="noopener">
             <img src="/static/avpro-logo.svg" alt="AV Pro logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.snapav.com/shop/en/snapav/wattbox" target="_blank">
+          <a
+            href="https://www.snapav.com/shop/en/snapav/wattbox"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/wattbox-logo.svg" alt="Wattbox logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.snapav.com/shop/en/snapav/binary" target="_blank">
+          <a
+            href="https://www.snapav.com/shop/en/snapav/binary"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/binary-logo.svg" alt="Binary logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://sound-machine.com/" target="_blank">
+          <a href="https://sound-machine.com/" target="_blank" rel="noopener">
             <img src="/static/soundmachine-logo.svg" alt="Soundmachine logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="http://www.ovrc.com/" target="_blank">
+          <a href="http://www.ovrc.com/" target="_blank" rel="noopener">
             <img src="/static/ovrc-logo.svg" alt="OVRC logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.snapav.com/shop/en/snapav/strong" target="_blank">
+          <a
+            href="https://www.snapav.com/shop/en/snapav/strong"
+            target="_blank"
+            rel="noopener"
+          >
             <img src="/static/strong-logo.svg" alt="Strong logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://www.sunbritetv.com/" target="_blank">
+          <a href="https://www.sunbritetv.com/" target="_blank" rel="noopener">
             <img src="/static/sunbrite-logo.svg" alt="Sunbrite logo" />
           </a>
         </div>
         <div className="col-sm-4 col-xs-6 product">
-          <a href="https://nest.com/" target="_blank">
+          <a href="https://nest.com/" target="_blank" rel="noopener">
             <img src="/static/nest-logo.svg" alt="Nest logo" />
           </a>
         </div>
       </div>
       <div className="dealer-status">
         <div>
-          <img src="/static/dealer-status-logos-01.svg" alt="Control 4 Authorized Gold Dealer 2014" />
+          <img
+            src="/static/dealer-status-logos-01.svg"
+            alt="Control 4 Authorized Gold Dealer 2014"
+          />
         </div>
         <div>
-          <img src="/static/dealer-status-logos-02.svg" alt="Control 4 Authorized Gold Dealer 2015" />
+          <img
+            src="/static/dealer-status-logos-02.svg"
+            alt="Control 4 Authorized Gold Dealer 2015"
+          />
         </div>
         <div>
-          <img src="/static/dealer-status-logos-03.svg" alt="Control 4 Authorized Gold Dealer 2016" />
+          <img
+            src="/static/dealer-status-logos-03.svg"
+            alt="Control 4 Authorized Gold Dealer 2016"
+          />
         </div>
         <div>
-          <img src="/static/dealer-status-logos-04.svg" alt="Control 4 Authorized Gold Dealer 2017" />
+          <img
+            src="/static/dealer-status-logos-04.svg"
+            alt="Control 4 Authorized Gold Dealer 2017"
+          />
         </div>
         <div>
-          <img src="/static/dealer-status-logos-05.svg" alt="Control 4 Authorized Gold Dealer 2018" />
+          <img
+            src="/static/dealer-status-logos-05.svg"
+            alt="Control 4 Authorized Gold Dealer 2018"
+          />
         </div>
         <div>
-          <img src="/static/dealer-status-logos-06.svg" alt="Control 4 Authorized Platinum Dealer 2019" />
+          <img
+            src="/static/dealer-status-logos-06.svg"
+            alt="Control 4 Authorized Platinum Dealer 2019"
+          />
         </div>
       </div>
     </div>
